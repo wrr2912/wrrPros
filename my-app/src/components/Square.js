8206bde8685f1844eee1486dca35038221705a59
wrr2912 <free2912521@sina.com>
2017-08-15 17:20:17 +0800

    all

---
+++ my-app/src/components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
